e43e7feb3180f0e35db6f62873f35e0bcc472a8b
GARCIA VENTURA <alu3934@cc203.(none)>
2014-10-17 15:05:57 +0100

    Test prueba suma

--- tc_complejo.rb
+++ tc_complejo.rb
@@ -6,4 +6,8 @@ class Test_Imaginario < Tets::Unit::TestCase
 	def test_mostrar
 		assert_equal("1 1i", Complejo.new(1,1).to_s)
 	end
+
+	def test_suma
+		assert_equal("2 2i", Fraccion.new(1,1).suma(1,1).to_s)
+	end
 end
---
+++ tc_complejo.rb~
@@ -0,0 +1,9 @@
+require "./imaginario.rb"
+require "test/unit"
+
+class Test_Imaginario < Tets::Unit::TestCase
+	
+	def test_mostrar
+		assert_equal("1 1i", Complejo.new(1,1).to_s)
+	end
+end
