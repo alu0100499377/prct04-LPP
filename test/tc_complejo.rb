f9849a384800d4ed5752333894564bc52b1f6aef
GARCIA VENTURA <alu3934@cc203.(none)>
2014-10-17 15:27:49 +0100

    Fichero test

---
+++ test/tc_complejo.rb
@@ -0,0 +1,17 @@
+require "./imaginario.rb"
+require "test/unit"
+
+class Test_Imaginario < Tets::Unit::TestCase
+	
+	def test_mostrar
+		assert_equal("1 1i", Complejo.new(1,1).to_s)
+	end
+
+	def test_suma
+		assert_equal("2 2i", Fraccion.new(1,1).suma(1,1).to_s)
+	end
+
+	def test_resta
+		assert_equal("2 2i", Fraccion.new(2,2).resta(1,1).to_s)
+	end
+end
