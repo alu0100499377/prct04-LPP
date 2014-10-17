f0729430cbc5ed9ea9f2b199ae233dd3b4d3ad58
HITA MANSO <alu0100713213@ull.edu.es>
2014-10-17 15:09:59 +0100

    Creado la estructura de directorios

---
+++ src/complejo.rb
@@ -0,0 +1,47 @@
+class Complejo	
+	attr_accessor :real, :imag
+	
+	def initialize(real, imag) 
+		@real, @imag = real, imag
+	end
+
+	def to_s
+		"#{@real} #{@imag}i"
+	end
+
+	def suma(x, y)
+		a = @real + x
+		b = @imag + y
+		imaginario = Complejo.new(a, b)
+		return imaginario
+	end
+
+	def restar(x, y)
+		a = @real - x
+		b = @imag - y
+		imaginario = Complejo.new(a, b)
+		return imaginario
+	end
+
+	def producto(x, y)
+		a = @real*x-@imag*y
+		b = @real*y+@imag*x
+		imaginario = Complejo.new(a, b)
+		return imaginario
+	end
+
+	def division(x, y)
+		a = @real+x+@imag+y/x*x+y*y
+		b = @imag+x+@real+y/x*x+y*y
+		imaginario = Complejo.new(a, b)
+		return imaginario
+	end
+
+	def escalar(x, y)
+		a = @real*x
+		b = @imag*y
+		imaginario = Complejo.new(a, b)
+		return imaginario
+	end
+
+end
